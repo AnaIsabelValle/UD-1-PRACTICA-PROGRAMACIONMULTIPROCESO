9ea2d5c03255e818dfa5285b9b589d89c71693fc
AnaIsabelValle <usr3048@salesianos-lacuesta.net>
2023-11-04 18:55:08 +0000

    fix:Añadido código

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        String rutaLorem = "C:\\Users\\ANA\\IdeaProjects\\UD1-Practica-ProgramacionMultiproceso\\src\\lorem_ipsum.txt";
+        String rutaLorem = "C:\\Users\\ANA\\IdeaProjects\\UD-1-Practica-ProgramacionMultiproceso\\src\\lorem_ipsum";
 
         LanzadorDeProcesos.iniciarContador(rutaLorem, "a", "salidaVocalA");
         LanzadorDeProcesos.iniciarContador(rutaLorem, "e", "salidaVocalE");
@@ -15,21 +15,46 @@ public class Main {
         LanzadorDeProcesos.iniciarContador(rutaLorem, "o", "salidaVocalO");
         LanzadorDeProcesos.iniciarContador(rutaLorem, "u", "salidaVocalU");
 
-        String rutaSalidaA = "C:\\Users\\ANA\\IdeaProjects\\UD1-Practica-ProgramacionMultiproceso\\src\\net\\salesianos\\salidas\\salidaVocalA";
+        String rutaSalidaA = "C:\\Users\\ANA\\IdeaProjects\\UD-1-Practica-ProgramacionMultiproceso\\src\\net\\salesianos\\salidas\\salidaVocalA";
         FileReader fileReaderA = new FileReader(rutaSalidaA);
-        BufferedReader bufferedReader2 = new BufferedReader(fileReaderA);
-        String numeroA = bufferedReader2.readLine();
-
-
+        BufferedReader bufferedReaderVocalA = new BufferedReader(fileReaderA);
+        String numeroA = bufferedReaderVocalA.readLine();
 
         System.out.println(numeroA);
-        // System.out.println(numeroA +);
 
-        String rutaTexto = "C:\\Users\\ANA\\IdeaProjects\\UD1-Practica-ProgramacionMultiproceso\\src\\lorem_ipsum.txt";
+        String rutaSalidaE = "C:\\Users\\ANA\\IdeaProjects\\UD-1-Practica-ProgramacionMultiproceso\\src\\net\\salesianos\\salidas\\salidaVocalE";
+        FileReader fileReaderE = new FileReader(rutaSalidaE);
+        BufferedReader bufferedReaderVocalE = new BufferedReader(fileReaderE);
+        String numeroE = bufferedReaderVocalE.readLine();
+
+        System.out.println(numeroE);
+
+        String rutaSalidaI = "C:\\Users\\ANA\\IdeaProjects\\UD-1-Practica-ProgramacionMultiproceso\\src\\net\\salesianos\\salidas\\salidaVocalI";
+        FileReader fileReaderI = new FileReader(rutaSalidaI);
+        BufferedReader bufferedReaderVocalI = new BufferedReader(fileReaderI);
+        String numeroI = bufferedReaderVocalI.readLine();
+
+        System.out.println(numeroI);
+
+        String rutaSalidaO = "C:\\Users\\ANA\\IdeaProjects\\UD-1-Practica-ProgramacionMultiproceso\\src\\net\\salesianos\\salidas\\salidaVocalO";
+        FileReader fileReaderO = new FileReader(rutaSalidaO);
+        BufferedReader bufferedReaderVocalO = new BufferedReader(fileReaderO);
+        String numeroO = bufferedReaderVocalO.readLine();
+
+        System.out.println(numeroO);
+
+        String rutaSalidaU = "C:\\Users\\ANA\\IdeaProjects\\UD-1-Practica-ProgramacionMultiproceso\\src\\net\\salesianos\\salidas\\salidaVocalA";
+        FileReader fileReaderU = new FileReader(rutaSalidaU);
+        BufferedReader bufferedReaderVocalU = new BufferedReader(fileReaderU);
+        String numeroU = bufferedReaderVocalU.readLine();
+
+        System.out.println(numeroU);
+
+        String rutaTexto = "C:\\Users\\ANA\\IdeaProjects\\UD-1-Practica-ProgramacionMultiproceso\\src\\lorem_ipsum";
         FileReader fileReaderTexto = new FileReader(rutaTexto);
         BufferedReader bufferedReaderTexto = new BufferedReader(fileReaderTexto);
         String textoFinal = bufferedReaderTexto.readLine();
-        while(textoFinal !=null){
+        while (textoFinal != null) {
 
             System.out.println(textoFinal);
 
--- src/net/salesianos/LanzadorDeProcesos.java
+++ src/net/salesianos/LanzadorDeProcesos.java
@@ -8,9 +8,9 @@ public class LanzadorDeProcesos {
     public static void iniciarContador(String texto, String vocal, String nombreArchivo) throws IOException {
 
         ProcessBuilder processBuilder = new ProcessBuilder(
-                "java", "C:\\Users\\ANA\\IdeaProjects\\UD1-Practica-ProgramacionMultiproceso\\src\\net\\salesianos\\Contador.java", texto, vocal);
+                "java", "C:\\Users\\ANA\\IdeaProjects\\UD-1-Practica-ProgramacionMultiproceso\\src\\net\\salesianos\\Contador.java", texto, vocal);
 
-        String rutaDeSalida = "C:\\Users\\ANA\\IdeaProjects\\UD1-Practica-ProgramacionMultiproceso\\src\\net\\salesianos\\salidas";
+        String rutaDeSalida = "C:\\Users\\ANA\\IdeaProjects\\UD-1-Practica-ProgramacionMultiproceso\\src\\net\\salesianos\\salidas";
         processBuilder.redirectOutput(new File(rutaDeSalida, nombreArchivo));
         processBuilder.start();
     }
